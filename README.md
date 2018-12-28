75a62943c04f601ac62dd264daf8e87607dd5fce
Wei Cheng Guo <rychard@Weis-MacBook-Pro.local>
2018-12-27 21:40:19 -0500

    :racehorse:

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
-# BeginnerRedux
+# Beginner Redux
 [Completed Redux Tutorial](https://medium.freecodecamp.org/understanding-redux-the-worlds-easiest-guide-to-beginning-redux-c695f45546f6)
+
+### Run Application
+```
+npm i
+npm start
+```
---
+++ src/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import registerServiceWorker from './registerServiceWorker';
+import { store } from "./store";
+
+
+// ReactDOM.render(<App />, document.getElementById('root'));
+const render = () => ReactDOM.render(<App />,
+   document.getElementById("root"));
+render();
+store.subscribe(render)
+registerServiceWorker();
